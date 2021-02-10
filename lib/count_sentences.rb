9c28c11ae15b475924fca30747be6042e731c2c5
nathan hains <nathanhains14@yahoo.com>
2021-02-10 18:44:03 -0500

    commit tree 46c295186a943fde0766d6362647678292ac190e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,43 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
+
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+      if self.end_with?("!")
+        true
+      else
+        false
+      end
   end
 
   def count_sentences
-
+   new_n = self.split
+   counter = 0
+   new_n.collect do |word|
+    if word.end_with?(".")
+      counter+=1
+    elsif word.end_with?("?")
+      counter+=1
+    elsif word.end_with?("!")
+      counter+=1
+    end
   end
+  counter
+end
+
 end
